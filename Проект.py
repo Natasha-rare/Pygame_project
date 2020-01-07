5b47f2759f7b904eecef313ff7664b2dc5c58144
Natasha-rare <natasha_ea5@mail.ru>
2020-01-07 13:51:28 +0300

    Работа с первым уровнем. Добавлены фигуры, которые появляются в рандомном месте поля при нажатии на 1. Фоигура останавливаются при пересечении с нижней границей, но наезжает на другую фигуру.

--- Проект.py
+++ Проект.py
@@ -1,13 +1,16 @@
 import os
 import sys
 import pygame
+from random import choice
 
 pygame.init()
 pygame.key.set_repeat(200, 70)
-FPS = 50
+FPS = 60
 WIDTH = 700
 HEIGHT = 600
 STEP = 10
+
+colors = ['blue', 'red', 'green', 'yellow', 'orange']
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
@@ -45,7 +48,8 @@ class Level(pygame.sprite.Sprite):
 
     def update(self, *args):
         if args and args[0].type == pygame.MOUSEBUTTONDOWN and self.rect.collidepoint(args[0].pos):
-            run_level(self.number)
+            if self.number == 1:
+                first_run()
 
 
 def load_image(name, color_key=None):
@@ -109,6 +113,43 @@ def start_screen():
         pygame.display.flip()
         clock.tick(FPS)
 
+class Figure(pygame.sprite.Sprite):
+    def __init__(self, x, y, n):
+        super().__init__(all_sprites)
+        # self.add(figures)
+        self.x = x
+        self.y = y
+        self.n = n
+        self.go = True
+        if n == 0:
+            self.image = pygame.Surface((80, 80))
+            self.rect = pygame.Rect(x, y, 80, 80)
+            pygame.draw.rect(self.image, pygame.Color(choice(colors)), (0, 0, 80, 80), 0)
+        elif n == 1:
+            self.image = pygame.Surface((40, 160))
+            self.rect = pygame.Rect(x, y, 40, 160)
+            pygame.draw.rect(self.image, pygame.Color(choice(colors)), (0, 0, 40, 160), 0)
+        elif n == 2:
+            self.image = pygame.Surface((80, 120))
+            self.rect = pygame.Rect(x, y, 80, 120)
+            pygame.draw.polygon(self.image, pygame.Color(choice(colors)), [(0, 0),
+                                                                           (40, 0), (40, 80), (80, 80),
+                                                                           (80, 120), (0, 120)], 0)
+
+    def update(self, flag, *args):
+        if (self.n == 0 and self.rect.y == 410) or \
+                (self.n == 1 and self.rect.y == 330) or \
+                (self.n == 2 and self.rect.y == 370):
+            self.go = False
+        if self.go:
+            if flag == 1:
+                self.rect.y += args[0]
+            else:
+                if args and args[0].type == key[pygame.K_RIGHT]:
+                    self.rect.x += 40
+
+
+
 class Board:
     # создание поля
     def __init__(self, width, height):
@@ -116,9 +157,9 @@ class Board:
         self.height = height
         self.board = [[0] * width for _ in range(height)]
         # значения по умолчанию
-        self.left = 5
-        self.top = 5
-        self.cell_size = 35
+        self.left = 10
+        self.top = 10
+        self.cell_size = 40
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -148,25 +189,60 @@ def show_levels():
         clock.tick(FPS)
 
 
-def run_level(level):
-    screen.fill((0, 0, 0))
+#def run_level(level):
+    #screen.fill((0, 0, 0))
+    #board.render(screen)
+    #font = pygame.font.Font(None, 30)
+    #text = font.render(f'Уровень {level}', 1, (255, 255, 100))
+    #screen.blit(text, (570, 10))
+    #exit_btn.draw(screen)
+    #while True:
+    #    for event in pygame.event.get():
+    #        if event.type == pygame.QUIT:
+    #            terminate()
+    #        if event.type == pygame.MOUSEBUTTONDOWN:
+    #            exit_btn.update(event)
+    #    if level == 1:
+    #       first_run()
+    #    pygame.display.flip()
+    #    clock.tick(FPS)
+
+
+v = 100
+f = 0
+# первый уровень
+def first_run():
     board.render(screen)
-    font = pygame.font.Font(None, 30)
-    text = font.render(f'Уровень {level}', 1, (255, 255, 100))
-    screen.blit(text, (570, 10))
-    exit_btn.draw(screen)
+    global key, f
     while True:
+        key = pygame.key.get_pressed()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
             if event.type == pygame.MOUSEBUTTONDOWN:
                 exit_btn.update(event)
+            if event.type == pygame.KEYDOWN:
+                figures.update(0, event)
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+                figures.add(Figure(10 + f * 40, 10, f % 3))
+                all_sprites.add(Figure(10 + f * 40, 10, f % 3))
+                f = choice(range(11))
+        screen.fill((0, 0, 0))
+        font = pygame.font.Font(None, 30)
+        text = font.render(f'Уровень 1', 1, (255, 255, 100))
+        screen.blit(text, (570, 10))
+        exit_btn.draw(screen)
+        figures.draw(screen)
+        y = v / FPS
+        figures.update(1, y)
+        board.render(screen)
         pygame.display.flip()
         clock.tick(FPS)
 
 
 all_sprites = pygame.sprite.Group()
 levels = pygame.sprite.Group()
+figures = pygame.sprite.Group()
 exit_btn = pygame.sprite.Group()
 level1 = Level(30, 80, 1)
 level2 = Level(180, 80, 2)
@@ -180,7 +256,7 @@ all_sprites.add(level2)
 levels.add(level2)
 all_sprites.add(level3)
 levels.add(level3)
-board = Board(16, 16)
+board = Board(11, 12)
 pygame.mixer.music.load('fon.mp3')
 pygame.mixer.music.play()
 running = True
